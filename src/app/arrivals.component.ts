710e43cf145531e8cd3b50d240e2644ba052c87a
FranzKieviet <franzkieviet@gmail.com>
2024-07-10 23:15:18 -0700

    Fixed logic and HTML for emojis

--- src/app/arrivals.component.ts
+++ src/app/arrivals.component.ts
@@ -55,11 +55,10 @@ interface Estimate {
 
   interface Daily {
     time: string[];
+    weather_code: number[];
     temperature_2m_max: number[];
     temperature_2m_min: number[];
     precipitation_sum: number[];
-    rain_sum: number[];
-    showers_sum: number[];
   }
 
   const DEFAULTSTATIONABREV = 'LAKE'
@@ -85,10 +84,11 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
     stationName: string | undefined;
     currentTime: string | undefined;
 
-    week: string[] | undefined;
-    highs: number[] = [0,0,0,0,0,0];
-    lows: number[] = [0,0,0,0,0,0];
-    icons: string[] = ['rainy', 'rainy', 'sunny', 'sunny', 'rainy'];
+    week: string[] = [];
+    highs: number[] = [];
+    lows: number[] = [];
+    rainOdds: number[] = [];
+    icons: string[] = [];
 
     //We want to check for changes, and left app.component know
     @Output() stationNameChange: EventEmitter<string> = new EventEmitter<string>();
@@ -116,7 +116,7 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
 
     async fetchWeather(lat: string, long:string) {
         //The key below is a public key
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ lat +'&longitude='+long+'&current=temperature_2m&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,showers_sum&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FLos_Angeles');
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ lat +'&longitude='+long+'&current=temperature_2m&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FLos_Angeles');
     
         if (!response.ok) {
             throw new Error('Network response was not ok' + response.statusText);
@@ -132,20 +132,57 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
     
     //Zipcode to Lat, Long
     //TODO
-    getFillWidth(low: number, high: number): string {
-      // Calculate the width as a percentage based on the temperature range
-      const tempRange = high - low;
-      const maxTempRange = 20; // Adjust this based on your expected temperature range
-      const percentage = (tempRange / maxTempRange) * 100;
-      return `${percentage}%`;
-    }
 
-    //Get the highs and lows:
-    updateHighsAndLows(){
-      
-      this.week = [];
-      this.highs = [];
-      this.lows = [];
+    //This fucntion helps figure out which emoji to use
+    //WeatherAPI gives the weather code, which is defined here:
+    //https://www.nodc.noaa.gov/archive/arc0021/0002199/1.1/data/0-data/HTML/WMO-CODE/WMO4677.HTM
+    updateIcons(code: number): string {
+      const weather_codes: { [key: string]: number[] } = {
+          "sunny": [0],
+          "partlyCloudy": [1, 2, 3, 4],
+          "mostlyCloudy": [5, 6],
+          "partlySunny": [7, 8],
+          "lightRainShowers": [21, 22, 23, 24, 25, 26, 27, 28, 29, 56, 57, 60, 61, 62, 63, 64, 65, 66, 67, 80, 81, 82],
+          "rainy": [12, 13, 14, 15, 16, 17, 18, 19, 20, 58, 59, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 83, 84, 85, 86, 87],
+          "thunderstorms": [29, 91, 92, 93, 94, 95, 96, 97, 98, 99],
+          "lightning": [93, 94],
+          "snowy": [71, 73, 74, 75, 85, 86, 87, 88, 89, 90],
+          "tornado": [99],
+          "foggy": [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+          "snowflakes": [71, 73, 74, 85, 86, 87]
+      };
+  
+      if (weather_codes["sunny"].includes(code)) {
+          return "sunny";
+      } else if (weather_codes["partlyCloudy"].includes(code)) {
+          return "partlyCloudy";
+      } else if (weather_codes["mostlyCloudy"].includes(code)) {
+          return "mostlyCloudy";
+      } else if (weather_codes["partlySunny"].includes(code)) {
+          return "partlySunny";
+      } else if (weather_codes["lightRainShowers"].includes(code)) {
+          return "lightRainShowers";
+      } else if (weather_codes["rainy"].includes(code)) {
+          return "rainy";
+      } else if (weather_codes["thunderstorms"].includes(code)) {
+          return "thunderstorms";
+      } else if (weather_codes["lightning"].includes(code)) {
+          return "lightning";
+      } else if (weather_codes["snowy"].includes(code)) {
+          return "snowy";
+      } else if (weather_codes["tornado"].includes(code)) {
+          return "tornado";
+      } else if (weather_codes["foggy"].includes(code)) {
+          return "foggy";
+      } else if (weather_codes["snowflakes"].includes(code)) {
+          return "snowflakes";
+      } else {
+          return "unknown";
+      }
+  }
+  
+    //Get the highs and lows and rain:
+    updateWeeklyWeather(){
       const data = this.forecastData
       
       if (data == undefined){
@@ -157,8 +194,8 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
         this.week.push(this.getDayOfWeek(data.daily.time[i]));
         this.highs.push(Math.round(data.daily.temperature_2m_max[i]));
         this.lows.push(Math.round(data.daily.temperature_2m_min[i]));
+        this.icons.push(this.updateIcons(data.daily.weather_code[i]));
       }
-      console.log(this.week)
       return;
     }
 
@@ -168,23 +205,6 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
       const dayIndex = date.getDay();
       return daysOfWeek[dayIndex];
   }
-    //Get the Date from the weatherAPI:
-    parseDate(date: string){
-      //Example from API: 2024-07-05
-      let year = parseInt(date.substring(0, 4), 10);
-      let month = parseInt(date.substring(5, 7), 10);
-      let day = parseInt(date.substring(8, 10), 10);
-
-      if(month < 10 ){
-        month = month % 10;
-      }
-
-      //If we want to return Date object instead:
-      //let month = parseInt(date.substring(5, 7), 10) - 1; //Months are 0-indexed :/
-      //const newDate = new Date(year, month, day);
-
-      return String(month) + "/" + String(day)
-    }
 
     ngOnInit() {
       this.initializeApp();
@@ -212,7 +232,7 @@ export class ArrivalsComponent implements OnInit, OnDestroy{
         this.cookieService.set('stationAbrev', DEFAULTSTATIONABREV);
       }
 
-      this.updateHighsAndLows();
+      this.updateWeeklyWeather();
       console.log("Intial init complete");
     }
 
